d625c55267502f9e80a29ec93c2c7ab72f88d04d
Mike Carey <MLCarey321@gmail.com>
2017-12-05 23:44:26 -0600

    Day Six

--- day_06.py
+++ day_06.py
@@ -2,33 +2,33 @@
 
 puzzle = str(input("? "))
 
-blocks = map(int, puzzle.split())
+banks = map(int, puzzle.split())
 combos = [''.join(puzzle)]
 magic_combo = ""
 
 
-def get_attempt_count(f):
-    attempts = 0
+def get_cycle_count(f):
+    cycles = 0
     while True:
-        attempts += 1
-        amount = max(blocks)
-        index = blocks.index(amount)
-        divvy = max(amount / len(blocks), 1)
-        blocks[index] = 0
-        given = 0
-        while given < amount:
-            index = (index + 1) % len(blocks)
-            blocks[index] += divvy
-            given += divvy
-            if divvy > (amount - given):
+        cycles += 1
+        max_blocks = max(banks)
+        index = banks.index(max_blocks)
+        divvy = max(max_blocks / len(banks), 1)
+        banks[index] = 0
+        redistributed = 0
+        while redistributed < max_blocks:
+            index = (index + 1) % len(banks)
+            banks[index] += divvy
+            redistributed += divvy
+            if divvy > (max_blocks - redistributed):
                 divvy = 1
-        combo = ''.join(map(str, blocks))
+        combo = ''.join(map(str, banks))
         if f(combo):
             break
         combos.append(combo)
-    return attempts
+    return cycles
 
 
-print "Part One", get_attempt_count((lambda c: c in combos))
-magic_combo = ''.join(map(str, blocks))
-print "Part Two", get_attempt_count((lambda c: c == magic_combo))
+print "Part One", get_cycle_count((lambda c: c in combos))
+magic_combo = ''.join(map(str, banks))
+print "Part Two", get_cycle_count((lambda c: c == magic_combo))
